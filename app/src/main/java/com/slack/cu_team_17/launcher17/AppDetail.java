7ad28f0b90f76d65275fc4599df034dc1eed8457
Owen Wattenmaker <owattenmaker@Owens-MacBook-Pro.local>
2018-01-22 16:10:22 -0700

    initial launcher

---
+++ app/src/main/java/com/slack/cu_team_17/launcher17/AppDetail.java
@@ -0,0 +1,9 @@
+package com.slack.cu_team_17.launcher17;
+
+import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
---
+++ app/src/main/java/com/slack/cu_team_17/launcher17/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.slack.cu_team_17.launcher17;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class HomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void showApps(View v) {
+        Intent i = new Intent(this, AppsListActivity.class);
+        startActivity(i);
+    }
+}
